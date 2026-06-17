d7bca277f6b58df1dc37010c5f2dadc439c2a2de
@x10an14-nav <christian.chavez@nav.no>
2026-06-17 14:06:05 +0200

    fix(sbom): update inputs, fix formatting, add direnv support, and fix sbom bundling

--- flake.nix
+++ flake.nix
@@ -4,122 +4,154 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    bombon = {
+      url = "github:nikstur/bombon";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, ... }@inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import inputs.nixpkgs { inherit system; };
-        inherit (pkgs) lib;
+        image-contents = pkgs.buildEnv {
+          name = "packages";
+          paths =
+            let
+              extra = pkgs.writeShellScriptBin "go" ''
+                #!/bin/sh
+                adventure
+              '';
+              motd = pkgs.writeTextDir "/home/nais/README" ''
+                Nais debug shell.
+                If you'd like some additional tool or have comments:
+                https://github.com/nais/debug/issues
 
-        dockerTag = if lib.hasAttr "rev" self then
-          "${builtins.toString self.revCount}-${self.shortRev}"
-        else
-          "gitDirty";
-        # Compile workspace code (including 3rd party dependencies)
-      in {
-        packages = rec {
-          default = image;
-          image = pkgs.dockerTools.buildImage {
-            name = "europe-north1-docker.pkg.dev/nais-io/nais/images/debug";
-            tag = "latest";
+                You have an unsettling feeling that you’ve been here before.
+                You see you have curl and openssl, there's a heap of binaries in /bin.
+                There's a door to the west
 
-            runAsRoot = ''
-              ${pkgs.dockerTools.shadowSetup}
-              groupadd -r nais
-              groupadd -g 65535 nobody
-              useradd -r -g nais -G nobody -u 1069 -d /home/nais -m nais
-              chown nais:nais /home/nais
-              chmod +w /home/nais
-            '';
-            copyToRoot = pkgs.buildEnv {
-              name = "packages";
-              paths = let
-                extra = (pkgs.writeShellScriptBin "go" ''
-                  #!/bin/sh
-                  adventure
-                '');
-                motd = pkgs.writeTextDir "/home/nais/README" ''
-                  Nais debug shell.
-                  If you'd like some additional tool or have comments:
-                  https://github.com/nais/debug/issues
+              '';
+              profile = pkgs.writeTextDir "/etc/bashrc" ''
+                if [ -f /etc/motd ]; then
+                  cat /etc/motd
+                fi
+              '';
 
-                  You have an unsettling feeling that you’ve been here before.
-                  You see you have curl and openssl, there's a heap of binaries in /bin.
-                  There's a door to the west
+              networkTools = with pkgs; [
+                curlFull
+                dnsutils
+                iana-etc
+                inetutils
+                iproute2
+                lsof
+                netcat
+                nmap
+                openssl
+                socat
+                tcpdump
+                wget
+              ];
+              shellTools = with pkgs; [
+                bashInteractive
+                bsdgames
+                coreutils
+                extra
+                gnugrep
+                gnused
+                gnutar
+                guile
+                htop
+                jq
+                man-pages
+                mg
+                motd
+                procps
+                profile
+                ripgrep
+                su
+                unzip
+                util-linux
+                vim
+                which
+                yq
+                zip
+                zulu
+              ];
+              persistenceTools = with pkgs; [
+                litecli
+                pgcli
+                redis
+              ];
+              binaryTools = with pkgs; [ strace ];
+              dockerEnv = with pkgs; [
+                dockerTools.usrBinEnv
+                dockerTools.binSh
+                dockerTools.caCertificates
+                dockerTools.shadowSetup
+              ];
+              kafkaTools = [ pkgs.kcat ];
+            in
+            shellTools ++ binaryTools ++ dockerEnv ++ kafkaTools ++ networkTools ++ persistenceTools;
 
-                '';
-                profile = pkgs.writeTextDir "/etc/bashrc" ''
-                  if [ -f /etc/motd ]; then
-                    cat /etc/motd
-                  fi
-                '';
+          pathsToLink = [
+            "/bin"
+            "/etc"
+            "/home/nais"
+          ];
+        };
+        sbom = inputs.bombon.lib.${system}.buildBom image-contents { };
+        image = pkgs.dockerTools.buildImage {
+          name = "europe-north1-docker.pkg.dev/nais-io/nais/images/debug";
+          tag = "latest";
 
-                networkTools = with pkgs; [
-                  curlFull
-                  dnsutils
-                  iana-etc
-                  inetutils
-                  iproute2
-                  lsof
-                  netcat
-                  nmap
-                  openssl
-                  socat
-                  tcpdump
-                  wget
-                ];
-                shellTools = with pkgs; [
-                  bashInteractive
-                  bsdgames
-                  coreutils
-                  extra
-                  gnugrep
-                  gnused
-                  gnutar
-                  guile
-                  htop
-                  jq
-                  man-pages
-                  mg
-                  motd
-                  procps
-                  profile
-                  ripgrep
-                  su
-                  unzip
-                  util-linux
-                  vim
-                  which
-                  yq
-                  zip
-                  zulu
-                ];
-                persistenceTools = with pkgs; [
-                  litecli
-                  pgcli
-                  redis 
-                ];
-                binaryTools = with pkgs; [ strace ];
-                dockerEnv = with pkgs; [
-                  dockerTools.usrBinEnv
-                  dockerTools.binSh
-                  dockerTools.caCertificates
-                ];
-                kafkaTools = [ pkgs.kcat ];
-              in shellTools ++ binaryTools ++ dockerEnv ++ kafkaTools
-              ++ networkTools ++ persistenceTools;
-
-              pathsToLink = [ "/bin" "/etc" "/home/nais" ];
-            };
-            config = {
-              Workingdir = "/home/nais";
-              Cmd = [ "bash" ];
-              User = "1069";
-            };
+          runAsRoot = ''
+            ${pkgs.dockerTools.shadowSetup}
+            groupadd -r nais
+            groupadd -g 65535 nobody
+            useradd -r -g nais -G nobody -u 1069 -d /home/nais -m nais
+            chown nais:nais /home/nais
+            chmod +w /home/nais
+          '';
+          copyToRoot = image-contents;
+          config = {
+            Workingdir = "/home/nais";
+            Cmd = [ "bash" ];
+            User = "1069";
+            Labels.SBOM = pkgs.lib.readFile "${sbom}";
           };
         };
-
-        formatter = pkgs.nixfmt-rfc-style;
-      });
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          inputsFrom = [ image ];
+          packages = [ pkgs.sbomnix ];
+        };
+        formatter = inputs.treefmt-nix.lib.mkWrapper pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            prettier.enable = true;
+            statix.enable = true;
+            nixfmt.enable = true;
+            deadnix.enable = true;
+          };
+          settings.global.excludes = [
+            "*.md"
+            "*.lock"
+            "LICENSE"
+          ];
+        };
+        packages = {
+          inherit image sbom;
+          default = image;
+        };
+      }
+    );
 }
